bbd54f07a78dac831d2ff97b075abc1a68c8821c
4toblerone <sasa.s.trifunovic@gmail.com>
2014-09-02 02:50:09 +0200

    setup and readme added

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='Purple',
+	  version='0.1',
+	  description='Framework for building dsls',
+	  url='https://github.com/4toblerone/Purple',
+	  author='Sasa Trifunovic',
+	  author_email='sasa.s.trifunovic@gmail.com',
+	  license='BSD',
+	  install_requires = ['ply==3.4'],
+	  long_description = read('README.md'),
+	)
